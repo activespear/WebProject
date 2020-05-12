8c82dd12bdfa9f8e0c366a24f1a09bddb9613229
Dimas <kda709@yandex.ru>
2020-05-12 04:24:32 +0300

    made temp file for functions, which use data from the user

--- scrypt.py
+++ scrypt.py
@@ -1,7 +1,16 @@
 from flask import Flask, url_for, render_template
-import geocode_root
+import sqlite3
+from geocode_root import show_on_map
 
 app = Flask(__name__)
+con = sqlite3.connect('DostSeeker.db')
+cur = con.cursor()
+result = cur.execute("""SELECT form FROM Forms""").fetchall()
+for i in range(len(result)):
+    result[i] = result[i][0]
+result1 = cur.execute("""SELECT subject FROM subjects""").fetchall()
+for i in range(len(result1)):
+    result1[i] = result1[i][0]
 
 
 @app.route('/')
@@ -9,9 +18,10 @@ app = Flask(__name__)
 def index():
     param = {}
     param['cssloc'] = url_for('static', filename='css/style.css')
-    classes = [8, 9, 10]
-    subjects = ['asd', 'asd']
-    return render_template('main.html', **param, sub_list=subjects, classes_list=classes)
+    image = 'Photo.png'
+    classes = result
+    subjects = result1
+    return render_template('main.html', **param, sub_list=subjects, classes_list=classes, img=image)
 
 
 @app.route('/mail')
